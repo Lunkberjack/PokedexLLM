3436f7a1cba04366e8c85cf899438018b0626cbd
Lunkubuntu <l.le.mi.lunk15@gmail.com>
2023-03-07 21:30:59 +0100

    Ayuda interactiva añadida
    Para DIN

---
+++ app/src/main/java/com/example/pokedexllm/AyudaInteractiva.kt
@@ -0,0 +1,11 @@
+package com.example.pokedexllm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AyudaInteractiva : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ayuda_interactiva)
+    }
+}
